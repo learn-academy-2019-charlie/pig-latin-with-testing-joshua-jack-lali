538882af2e5feea1c2be09a0b9ce48f2ad0e702f
learn <learn@learn06.local>
2019-06-11 11:19:44 -0700

    solved most logic issues, stopped short of dealing with 'qu'

--- piglatin.test.js
+++ piglatin.test.js
@@ -9,30 +9,65 @@
 // note: treat words beginning with 'y' as consonant
 var input = "test input"
 var vowel = ["a","e","i","o","u"]
-var test = "eye apple owl towel"
+var test = "eye, apple! owl &towel/% there"
 
 const pigLatin = (str) => {
     let arr = str.split(" ")
-    let loc = []
-    let holder = arr.map(value => {
-        loc.push(value.indexOf(vowel[arr.map.key]))
-        return value
+    let holder = arr.map(value =>{
+        let tempStr = value.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+        let loc = tempStr.search(/[aeiou]/)
+        if (loc === 0){
+            return tempStr + "way"
+        } else {
+            let cut = tempStr.slice(0,loc) + "ay"
+            return tempStr.substring(loc) + cut
+        }
     })
-        console.log(holder);
-        console.log(loc);
-    let temp = holder.map(string => {
-        loc.map(element => {
-            if(element === 0){
-                return string + "way"
-            }else if(element > 0){
-                let pusher = string.slice(0,loc)
-                return string.slice(loc,-1) + pusher + "ay"
-            }else{
-                return string
-            }
-        })
-    })
-    return temp
+    var iterator1 = holder.values()
+    var iter = iterator1.next()
+    // while (iter.done != true){
+    //     //if(vowel.indexOf(iter.value[0]) != -1){
+    //         //loc.push(0)
+    //     loc.push(iter.value.search(/[aeiou]/))
+    //
+    //     iter = iterator1.next()
+    //}
+    console.log(holder);
 }
 
-console.log(pigLatin(test));
+
+
+pigLatin(test)
+
+
+
+
+
+
+
+
+// const pigLatin = (str) => {
+//     let arr = str.split(" ")
+//     let loc = []
+//     let holder = arr.map((value, index) => {
+//         loc.push(value.indexOf(vowel[arr.map.key]))
+//         return value
+//     })
+//         console.log(holder);
+//         console.log(loc);
+//     let temp = holder.map(string => {
+//         loc.map(element => {
+//             if(element === 0){
+//                 return string + "way"
+//             }else if(element > 0){
+//                 let pusher = string.slice(0,loc)
+//                 return string.slice(loc,-1) + pusher + "ay"
+//             }else{
+//                 return string
+//             }
+//         })
+//     })
+//     return temp
+// }
+//
+// console.log(pigLatin(test));
